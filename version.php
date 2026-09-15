9133680117e21f99c1ffa086a9b73f226da04b64
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-14 21:13:39 -0300

    V 1.1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = "mod_exiticket";
 $plugin->version = 2026091400;
-$plugin->release = "1.0.0";
+$plugin->release = "1.1.0";
+$plugin->component = "mod_exiticket";
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
